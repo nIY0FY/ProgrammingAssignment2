e3d0eaea170fa749712c34451fea2882dc845f9d
nIY0FY <nIY0FY@users.noreply.github.com>
2014-05-03 22:10:12 +0200

    Edit the R file and place solution
    Edit the R file contained in the git repository and place solution in that file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation, functions below are an approach
+## to save computing time by caching the inverse of a matrix, and an based upon function
+## to cache mean of vector provided by R.D. Peng
 
-## Write a short comment describing this function
-
+## 'makeCacheMatrix' creates an R object that can storage a matrix, its inverse
+## and methods to deal with them
 makeCacheMatrix <- function(x = matrix()) {
+    # This variable store the inverse of Matrix x
+    inverseMx <- NULL
+    
+    # 'set' sets a new matrix to compute its inverse
+    # and initialize the inverse Matrix, to be computed
+    set <- function(y) {
+        x <<- y
+        inverseMx <<- NULL
+    }
 
+    # 'get' returns current matrix
+    get <- function() x
+
+    # This method store an inverse matrix computed
+    setInverse <- function(inverse) inverseMx <<- inverse
+
+    # This method returns an inverse matrix "cached" previouly
+    getInverse <- function() inverseMx
+    
+    # function returns a list
+    list(set = set
+        ,get = get
+        ,setInverse = setInverse
+        ,getInverse = getInverse
+    )
 }
 
-
-## Write a short comment describing this function
-
+## 'cacheSolve' returns inverse of an invertible matrix stored previously in an object
+## created with 'makeCacheMatrix', inverse is computed only during first execution
+## and the matrix has not changed.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Check if Matrix x was cached previously and return it in such case
+    inverseMx <- x$getInverse()
+    if(!is.null(inverseMx)) {
+        message("getting cached inverse of Matrix X...")
+        return(inverseMx)
+    }
+    
+    # Computing the inverse...
+    matrixX   <- x$get()
+    inverseMx <- solve(matrixX, ...)
+
+    # this call stores the inverse matrix and return it
+    x$setInverse(inverseMx)
+    inverseMx
 }
